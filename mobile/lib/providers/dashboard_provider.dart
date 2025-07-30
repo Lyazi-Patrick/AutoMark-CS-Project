f2b0e6a37bcfd13a6a237ecf7191b2dca9537190
Jovans-code <jvhan13@gmail.com>
2025-07-30 14:35:31 +0300

    Update dashboard_provider.dart

--- mobile/lib/providers/dashboard_provider.dart
+++ mobile/lib/providers/dashboard_provider.dart
@@ -18,7 +18,7 @@ class DashboardProvider extends ChangeNotifier {
       final currentUser = FirebaseAuth.instance.currentUser;
 
       if (currentUser == null) {
-        // No user logged in, clear statatistcs
+        // Incase of No user logged in, clear statatistcs
         marked = 0;
         unmarked = 0;
         total = 0;
